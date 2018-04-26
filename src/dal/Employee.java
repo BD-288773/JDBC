784c328a77b58b921f43e43f23c5d90610805914
BD-288773 <ssidoruk@mion.elka.pw.edu.pl>
2018-04-26 22:34:14 +0200

    added dal

---
+++ src/dal/Employee.java
@@ -0,0 +1,110 @@
+package dal;
+
+public class Employee {
+
+	public int employeeId;
+	public String firstName;
+	public String lastName;
+	public String email;
+	public String phoneNumber;
+	public String hireDate;
+	public String jobId;
+	public float salary;
+	public float commisionPct;
+	public int managerId;
+	public int departmentId;
+	
+	public Employee() {}
+	
+	public Employee(int eId) {
+		this.employeeId = eId;
+	}
+
+	public int getEmployeeId() {
+		return employeeId;
+	}
+
+	public void setEmployeeId(int employeeId) {
+		this.employeeId = employeeId;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+
+	public String getHireDate() {
+		return hireDate;
+	}
+
+	public void setHireDate(String hireDate) {
+		this.hireDate = hireDate;
+	}
+
+	public String getJobId() {
+		return jobId;
+	}
+
+	public void setJobId(String jobId) {
+		this.jobId = jobId;
+	}
+
+	public float getSalary() {
+		return salary;
+	}
+
+	public void setSalary(float salary) {
+		this.salary = salary;
+	}
+
+	public float getCommisionPct() {
+		return commisionPct;
+	}
+
+	public void setCommisionPct(float commisionPct) {
+		this.commisionPct = commisionPct;
+	}
+
+	public int getManagerId() {
+		return managerId;
+	}
+
+	public void setManagerId(int managerId) {
+		this.managerId = managerId;
+	}
+
+	public int getDepartmentId() {
+		return departmentId;
+	}
+
+	public void setDepartmentId(int departmentId) {
+		this.departmentId = departmentId;
+	}
+}
